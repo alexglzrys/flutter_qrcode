fc5931151ebc254c65d3762cc2ab76c0a406232a
Alejandro González Reyes <alexglzrys@gmail.com>
2024-03-29 17:23:46 -0600

    modelo para manejar los Scans

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+// Modelo generado desde https://app.quicktype.io/
+
+// Modelo para representar un recurso escaneado desde un código QR (sitio Web o Coordenadas de Geolocalización)
+class ScanModel {
+  // El identificador del recurso
+  int? id;
+  // http (sitio Web) | geo (Geolocalización)
+  String type;
+  // Uri | geolocation
+  String value;
+
+  ScanModel({
+    this.id,
+    required this.type,
+    required this.value,
+  }) {
+    // Inicializar el tipo de recurso escaneado (solo se permiten direcciones Web o coordenadas de geolocalización)
+    if (value.contains('http')) {
+      type = 'http';
+    } else {
+      type = 'geo';
+    }
+  }
+
+  factory ScanModel.fromRawJson(String str) =>
+      ScanModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  // Codificador
+  // Constructor para generar una instancia de ScanModel a partir de un Mapa (que cumpla con la estructura JSON)
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+      );
+
+  // Decodificador
+  // Convertir una instancia de ScanModel a un Mapa
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "value": value,
+      };
+}
