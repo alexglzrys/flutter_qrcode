a12541e5b50d908959bdad38e34327eee2cd7dbd
Alejandro González Reyes <alexglzrys@gmail.com>
2024-03-31 18:34:15 -0600

    scaffold UI - listado de scans de tipo mapa

--- lib/partials/maps_view.dart
+++ lib/partials/maps_view.dart
@@ -7,6 +7,17 @@ class MapsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Maps View'));
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.map_outlined, color: Colors.cyan),
+          title: const Text('http://localhost.com'),
+          subtitle: const Text('ID: 251'),
+          trailing: const Icon(Icons.arrow_forward_ios_outlined),
+          onTap: () => print('scan presionado...'),
+        );
+      },
+      itemCount: 10,
+    );
   }
 }
