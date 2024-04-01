b3699d87b7845c855af4dbe5352aa1505a61d0d0
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-01 17:53:01 -0600

    cambiar el tipo de mapa y punto central

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:qr_code_flutter_app/models/scan_model.dart';
+import 'package:qr_code_flutter_app/utils/utils.dart';
 
 // Widget para mostrar una ubicación en un mapa de Google
 // El paquete google_maps_flutter requiere trabajar con widgets con estado para manejar de manera efectiva la interacción del usuario, la actualización dinámica del mapa y el ciclo de vida del mapa, lo que proporciona una experiencia de usuario fluida y optimizada.
@@ -17,23 +18,28 @@ class _MapScreenState extends State<MapScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  // Define la posición inicial de la cámara en un mapa de Google. Se utiliza para especificar la ubicación y la orientación de la cámara en el mapa.
-  // Esta posición se puede usar al iniciar el mapa para que el usuario vea una ubicación específica y con un nivel de zoom determinado.
-  final CameraPosition initialPosition = const CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
   @override
   Widget build(BuildContext context) {
     // Recuperar los argumentos enviados en la ruta (scan de geolocalización seleccionado) asociada con esta pantalla
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
+
+    // Define la posición inicial de la cámara en un mapa de Google. Se utiliza para especificar la ubicación y la orientación de la cámara en el mapa.
+    // Esta posición se puede usar al iniciar el mapa para que el usuario vea una ubicación específica y con un nivel de zoom determinado.
+    final CameraPosition initialPosition = CameraPosition(
+      // Usar el método utilitario para proporcionar la Latitud y Longitud en el formato compatible que espera la librería de GoogleMpasFlutter
+      target:
+          getLatLng(scan.value), // Coordenadas a mostrar como centro del mapa
+      zoom: 17.5, // La cercanía al mapa
+      tilt: 50, // Inclinación o perspectiva del mapa
+    );
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Ubicación')),
+      appBar: AppBar(title: const Text('Ubicación en mapa')),
       // Widget de terceros para mostrar una geolocalización en un mapa de Google
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        myLocationButtonEnabled: false,
+        mapType: MapType.normal,
         initialCameraPosition: initialPosition,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,4 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // Método utilitarion para lanzar el navegador hacia una determinada URL
@@ -5,4 +6,15 @@ Future<void> launchInWebView(Uri uri) async {
   if (!await launchUrl(uri, mode: LaunchMode.inAppWebView)) {
     throw Exception('No se puede lanzar la URL $uri');
   }
+}
+
+// Méotodo utilitario para obtener la latitud y longitud en formato compatible con Google Maps
+LatLng getLatLng(String value) {
+  // geo:75.88911,-99.181515
+
+  // Obtener la latitud y longitud en una lista [75.88911,-99.181515]
+  final List coordinates = value.substring(4).split(',');
+  final double latitude = double.parse(coordinates[0]);
+  final double longitude = double.parse(coordinates[1]);
+  return LatLng(latitude, longitude);
 }
