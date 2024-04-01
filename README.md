7e40b6a142e189aa166df97f70503141f39c65ce
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-01 17:31:26 -0600

    mostrar Google Maps en Android

--- README.md
+++ README.md
@@ -12,3 +12,20 @@ A new Flutter project.
 flutter run --multidex
 ```
 
+# Habilitar el SDK de Google Maps en Android
+
+- Ir a la consola de Google Developers **https://console.cloud.google.com**
+- Seleccionar el proyecto dónde se desea habilitar Google Maps
+- Seleccionar del menú principal la opción **APIs y servicios** - **Biblioteca**
+- Seleccionar y Habilitar **Google Maps SDK Android**
+- *Nota: Cada Plataforma requiere habilitar su respectiva SDK*
+
+Google Maps ha cambiado recientemente sus políticas de uso, ahora exige a los usuarios a facturar por el uso de su SDK al momento de integrarla en sus respectivas aplicaciones, **Google Maps nos regala $200 US en creditos para usar su servicio durante el mes actual, al finalizar el mes, nos vuelve a regalar otros $200 US en crédito y así sucesivamente**. Eso equivale a unas 28,500 solicitudes de cardas de mapas sin costo, por lo que es más que suficiente al momento de programar. En caso de exceder este crédito, Google comienza a facturar con cargo a la tarjeta registrada durante el proceso de integración y registro del SDK de Google Maps.
+
+# Generar API Keys y asociarlas a cada SDK
+
+- Seleccionar del menú principal la opción **APIs y servicios** - **Credenciales**
+- Crear credenciales
+- Seleccionar **Clave de API**
+- Seleccionar la nueva API Key generada, y proceder a configurarla y asociarla con su respectiva SDK
+- Se recomienda que cada API Key se asocie con un producto o servicio en particualar. 
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,14 +1,44 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:qr_code_flutter_app/models/scan_model.dart';
 
-class MapScreen extends StatelessWidget {
+// Widget para mostrar una ubicación en un mapa de Google
+// El paquete google_maps_flutter requiere trabajar con widgets con estado para manejar de manera efectiva la interacción del usuario, la actualización dinámica del mapa y el ciclo de vida del mapa, lo que proporciona una experiencia de usuario fluida y optimizada.
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  // En el contexto de Google Maps en Flutter, este Completer se utiliza típicamente para esperar a que el widget GoogleMap esté completamente inicializado antes de interactuar con él.
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  // Define la posición inicial de la cámara en un mapa de Google. Se utiliza para especificar la ubicación y la orientación de la cámara en el mapa.
+  // Esta posición se puede usar al iniciar el mapa para que el usuario vea una ubicación específica y con un nivel de zoom determinado.
+  final CameraPosition initialPosition = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
     // Recuperar los argumentos enviados en la ruta (scan de geolocalización seleccionado) asociada con esta pantalla
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
-    return Scaffold(body: Center(child: Text(scan.value)));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ubicación')),
+      // Widget de terceros para mostrar una geolocalización en un mapa de Google
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
   }
 }
